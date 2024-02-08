aa38d941492245171b1569b20bf84c9bdb853f5f
BISMA-IBRAHIM <bismaibrahim500@gmail.com>
2024-02-09 01:56:18 +0500

    bhai meri file upload kro shabashh

---
+++ 02leapyear.js
@@ -0,0 +1,16 @@
+var year = prompt("Enter Year And Check Its leap Or Not");
+if(year % 4==0){
+    document.write("its leap year"+ year);
+
+}else{
+    document.write("its not leap year"+year);
+}
+
+
+
+
+
+var year = prompt("Enter Year And Check Its leap Or Not");
+if(year % 4==0){
+    document.write("its leap year" +year);
+}else{document.write("its not leap year")}
